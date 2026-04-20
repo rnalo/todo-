3de4f83b096016e498f4dba890e03bae2ea43e0f
rnalo <tulpz814@icloud.com>
2026-04-20 19:31:43 +0900

    Implement task storage and display in TODO app
    Added functionality to store and display tasks in the TODO app.

--- todolist.py
+++ todolist.py
@@ -2,8 +2,12 @@ import streamlit as st
 
 st.title("📝 TODOアプリ")
 
-task = st.text_import("タスクを入力")
-add_button = st.button("追加")
+tasks = []
 
-if add_button:
-    st.write(f"{task}")
+task = st.text_import("タスクを入力")
+if st.button("追加"):
+   tasks.append(task)
+   st.write(f"{task}")
+
+
+st.write(task)
